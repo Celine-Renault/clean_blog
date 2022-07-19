6dcdeb991fdf744e4df994f72d7b376b88d7f528
Céline Renault <renault.celine@icloud.com>
2022-07-19 16:59:14 +0200

    page enregistrement verifiation email

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Egulias\EmailValidator\EmailValidator;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,7 +19,7 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class) // toujours precier le type d'input
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
