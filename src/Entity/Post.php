c6da8b51421954c5f6022f7b46b8c42e6e427f91
Céline Renault <renault.celine@icloud.com>
2022-08-29 09:47:51 +0200

    verification test like

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\PostRepository;
+// use App\Entity\Like;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
